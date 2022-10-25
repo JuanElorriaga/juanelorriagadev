262e02c2de2b34a3acce1273d09630f8b7d32a4b
Juan Elorriaga <39384464+JuanElorriaga@users.noreply.github.com>
2022-10-25 13:02:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-function sendEmail() {
-	Email.send({
-	Host: "smtp.mail.yahoo.com",
-	Username : "<juandevgithub@yahoo.com>",
-	Password : "<Senhapadrao2022>",
-	To : '<juandevgithub@yahoo.com>',
-	From : "<sender’s email address>",
-	Subject : "<Email de Contato>",
-	Body : "<email body>",
-	}).then(
-		message => alert("mail sent successfully")
-	);
+const form = document.querySelector('.contact_form');
+
+function senMsg(e){
+e.preventDefault();
+	
+const name = document.querySelector('.name');
+email= document.querySelector('.email');
+msg  = document.querySelector('.msg');
+
+
+
+
+//função para envio do email
+
+email.send({
+	
+	to: 'juangithubdev@yahoo.com',
+	from: email.value,
+	Subject: "Contact Form",
+	Body: msg.value	
+}).then(
+	message=> alert(message)
+);
+	
 }
+
+form.addEventListener('submit', sendMsg);
